d5c2115c522c525c1c2d42ce32653cf979e3decb
demi <phandiem0709@gmail.com>
2024-06-12 17:08:07 +0700

    modify main code to use recursion and to see green forever

--- src/main/java/com/demi/mathutil/core/MathUtility.java
+++ src/main/java/com/demi/mathutil/core/MathUtility.java
@@ -19,20 +19,43 @@ public class MathUtility {
     //n 0...20; do 21! tràn kiểu long, ko có âm giai thừa
     //0! = 1
     public static long getFactorial(int n){
-        //sửa lại từ 10 thành 1 để có màu xanh
-        long product = 1; //biến con heo đất, biến nhân dồn
         
         if (n < 0 || n > 20)
             throw new IllegalArgumentException("Invalid n. n must be between 0..20");
         
-        if (n==0)
-            return 1;
-        //code chạy đến đây, n = 1..2..20
-        for (int i = 1; i <= n; i++) {
-            product *=i; //product = product * i;
-        }
-        return product;
+        if (n == 0 || n == 1)
+            return 1; //điểm dần của đệ quy
+        
+        return n * getFactorial(n-1);
+        //n! = n * (n-1) // stack LIFO - Last in firset out
+        //Using Recursion to demostrate regression test
     }
 }
+
+//Học về  REGRESSION TEST - Kiểm thử hồi quy
+//Kiểm thử lại những thứ đã từng kiểm thử
+//Tại sao phải làm những điều này
+//Lý do: Code đang ổn thì sẽ luôn ổn nếu không ai đụng vào nó, nhưng nếu có fix bug, có sửa hàm, có tối ưu thuật toán
+ //trong hàm, thì hàm cần phải được test lại, để xác nhận nó còn ngon, còn xanh đỏ hay không sau khi code được sửa
+
+//Nếu có UNIT TEST CODE (JUNIT) việc test code lại cực nhanh
+//Chỉ cần thấy màu xanh của các UNIT TEST CASE được run là đủ 
+//Nhanh nữa: đưa unit test lên GitHub, cài CI SCRIPT (file yaml yml) vào mọi thứ tự động báo xanh đỏ luôn
+
+//Kiểm thử lại những thứ đã kiểm thử được gọi là kiểm thử hồi quy - REGRESSION TEST
+//Nên dùng tự động bằng cách xài Unit Test Framework
+//Nhìn bằng mắt tưg hàm main() cách truyền thống là không nên
+
+//5! = 1.2.3.4.5 = 1.2.3.4    .5
+//                  4!        .5
+// => 5! = 5.4!
+
+//4! = 1.2.3.4 => 4!=4.3!
+//....
+
+// => N! = N * (N-1)!
+//Công thức đệ quy - RECURSION - Gọi lại chính mình với quy mô nhỏ hơn
+//Giống con búp bê người Nga
+
 
 
